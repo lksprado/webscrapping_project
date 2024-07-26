3faae6bde1efb321cb51ea14d14b1a927ec3a2e7
lksprado <lks-prado@live.com>
2024-07-26 16:45:59 -0300

    updated class

--- src/product_reader.py
+++ src/product_reader.py
@@ -1,32 +1,50 @@
-import json
-import pandas as pd  
+# THIS SCRIPT READS EXCEL FILE THAT CONTAINS THE PRODUCTS THAT WILL BE SEARCHED IN THE SCRAPPER SCRIPT
 
-# THIS CODE IS STRAIGHT FORWARD TO COLLECT DESIRED PRODUTS FROM EXCEL FILE
+import json
+import pandas as pd
+import unicodedata
 
 class ProductReader:
-    '''INITIALIZES THE PRODUCTREAD WITH A CONFIGURATION FILE FOR ENVIRONMENT VARIABLES'''
     def __init__(self, config_file_path: str = './.git/config.json'):
-        self.config_file_path =  config_file_path
+        self.config_file_path = config_file_path
         self.config_data = None
         self.product_df = None
 
     def read_config(self):
-        '''READS CONFIGURATION FILE AND STORES IT MEMORY'''
         with open(self.config_file_path, 'r') as file:
             self.config_data = json.load(file)
-        
-    def read_product(self, sheet_name: str = 'Sheet1'):
-        '''READS EXCEL FILE WITH DESIRED PRODUCTS'''
-        product_file = self.config_data['product_path']
-        self.product_df = pd.read_excel(product_file,sheet_name=sheet_name)
-    
-    def standardize(self):
-        '''STANDARDIZES COLUMNS TO LOWER CASE'''
-        self.product_df[['Produto','Marca']] = self.product_df[['Produto','Marca']].apply(lambda x: x.str.lower())        
-        return self.product_df
 
-if __name__ == "__main__":
-    pr = ProductReader()
-    pr.read_config()
-    pr.read_product()
-    pr.standardize()
+    def read_product(self, sheet_name: str = 'Sheet1'):
+        product_file = self.config_data['product_path']
+        self.product_df = pd.read_excel(product_file, sheet_name=sheet_name)
+        
+    def read_sku(self,sheet_name: str = 'Sheet2'):
+        product_file = self.config_data['product_path']
+        self.sku_df = pd.read_excel(product_file, sheet_name=sheet_name)
+
+    def _remove_special_characters(self, text):
+        '''REMOVES ACCENTS AND SPECIAL CHARACTERS FROM TEXT, IF TEXT IS STRING'''
+        if isinstance(text, str):
+            text = unicodedata.normalize('NFKD', text)
+            text = ''.join([c for c in text if not unicodedata.combining(c)])
+        return text
+
+    def standardize_product(self):
+        '''MAKES DATA LOWER CASE, FILTERING, SELECT PRODUCT COLUMN ONLY, REMOVES SPECIAL CHARACTERS'''
+        # CONVERTS TO LOWER CASE
+        self.product_df['SEARCH_LIST'] = self.product_df['SEARCH_LIST'].apply(lambda x: x.lower() if isinstance(x, str) else x)
+        
+        # REMOVE SPECIAL CHARACTERS FROM 'SEARCH LIST' 
+        self.product_df['SEARCH_LIST'] = self.product_df['SEARCH_LIST'].apply(self._remove_special_characters)
+        
+        # REMOVE DUPLICATES IF ANY ON 'SEARCH_LIST' 
+        self.product_df.drop_duplicates(subset=['SEARCH_LIST'], inplace=True)
+        
+        # RETURN THE LIST OF UNIQUE ITEMS
+        return self.product_df['SEARCH_LIST'].tolist()
+    
+    def standardize_sku(self):
+        '''DROPS DUPLICATE SKU IF ANY'''
+        self.sku_df.drop_duplicates(subset=['SKU'], inplace=True)
+        return self.sku_df['SKU'].tolist()
+
